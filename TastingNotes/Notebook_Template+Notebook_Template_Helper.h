daf4a8caafa852770d37713c3681a0b8fb2cbd1b
Matt <matt@mobileappmastery.com>
2013-12-20 15:17:00 -0500

    Made core data notebook template sorted with category

---
+++ TastingNotes/Notebook_Template+Notebook_Template_Helper.h
@@ -0,0 +1,15 @@
+//
+//  Notebook_Template+Notebook_Template_Helper.h
+//  TastingNotes
+//
+//  Created by Matt on 12/20/13.
+//  Copyright (c) 2013 Mobile App Mastery. All rights reserved.
+//
+
+#import "Notebook_Template.h"
+
+@interface Notebook_Template (Notebook_Template_Helper)
+
+-(NSArray *) groupsByOrder;
+
+@end
---
+++ TastingNotes/Notebook_Template+Notebook_Template_Helper.m
@@ -0,0 +1,20 @@
+//
+//  Notebook_Template+Notebook_Template_Helper.m
+//  TastingNotes
+//
+//  Created by Matt on 12/20/13.
+//  Copyright (c) 2013 Mobile App Mastery. All rights reserved.
+//
+
+#import "Notebook_Template+Notebook_Template_Helper.h"
+
+@implementation Notebook_Template (Notebook_Template_Helper)
+
+-(NSArray *) groupsByOrder{
+    NSSortDescriptor *sorter = [NSSortDescriptor sortDescriptorWithKey:@"order"
+                                                             ascending:YES];
+
+    return [self.groups sortedArrayUsingDescriptors:@[sorter]];
+}
+
+@end
