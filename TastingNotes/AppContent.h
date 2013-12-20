775c2ec3f7948eab2065271eb13f94e63dad776a
Matt <matt@mobileappmastery.com>
2013-12-20 09:45:48 -0500

    Added Core Data

---
+++ TastingNotes/AppContent.h
@@ -0,0 +1,13 @@
+//
+//  AppContent.h
+//  TastingNotes
+//
+//  Created by Matt on 12/20/13.
+//  Copyright (c) 2013 Mobile App Mastery. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppContent : NSObject
+
+@end
---
+++ TastingNotes/AppContent.m
@@ -0,0 +1,13 @@
+//
+//  AppContent.m
+//  TastingNotes
+//
+//  Created by Matt on 12/20/13.
+//  Copyright (c) 2013 Mobile App Mastery. All rights reserved.
+//
+
+#import "AppContent.h"
+
+@implementation AppContent
+
+@end
