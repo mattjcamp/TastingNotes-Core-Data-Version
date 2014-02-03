48d7ec7ee22131d2fdf664e7bd0e9af80a22164b
Matt <matt@mobileappmastery.com>
2014-02-03 12:09:18 -0500

    Little bit more

--- TastingNotes/NoteCVC.m
+++ TastingNotes/NoteCVC.m
@@ -34,18 +34,6 @@
     
     cvc.myLabel.text = gt.name;
     
-    /*
-     NSMutableString *contentString = [[NSMutableString alloc]init];
-     
-     [gt.contentTypesByOrder enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
-     [contentString appendFormat:@"%@\n", [self.note contentInThisGroup:gt
-     andThisContentType:obj]];
-     }];
-     
-     cvc.myTextView.text = contentString;
-     */
-
-    
     //Add First View
     cvc.myScrollView.pagingEnabled = YES;
     CGFloat totalHeight = 0;
@@ -59,46 +47,18 @@
     [cvc.myScrollView addSubview:v];
     
     //Add Second View
-    y = y + v.frame.size.height + 5;
-    r = CGRectMake(cvc.frame.origin.x,
-                   y,
-                   cvc.frame.size.width,
-                   v.frame.size.height);
-    v = [ViewFactory testViewWithThisReferenceFrame:r];
-    totalHeight = totalHeight + v.frame.size.height;
-    [cvc.myScrollView addSubview:v];
+    for(int i = 0;i<80;i++){
+        y = y + v.frame.size.height + 5;
+        r = CGRectMake(cvc.frame.origin.x,
+                       y,
+                       cvc.frame.size.width,
+                       v.frame.size.height);
+        v = [ViewFactory testViewWithThisReferenceFrame:r];
+        totalHeight = totalHeight + v.frame.size.height;
+        [cvc.myScrollView addSubview:v];
+    }
     
     cvc.myScrollView.contentSize = CGSizeMake(cvc.contentView.frame.size.width, totalHeight);
-    
-    
-    /*    for (int i = 0; i < numberOfViews; i++) {
-     CGFloat y = cvc.frame.origin.y;
-     [cvc.myScrollView addSubview:[ViewFactory testViewWithThisReferenceFrame:cvc.frame]];
-     
-     }*/
-    
-    /*
-     for (int i = 0; i < numberOfViews; i++) {
-     CGFloat yOrigin = i * (cvc.contentView.frame.origin.y + cvc.contentView.frame.size.height);
-     CGRect r = CGRectMake(5, yOrigin, cvc.contentView.frame.size.width, cvc.contentView.frame.size.height);
-     UIView * awesomeView = [[UIView alloc] initWithFrame:r];
-     switch (i) {
-     case 0:
-     awesomeView.backgroundColor = [UIColor blueColor];
-     break;
-     case 1:
-     awesomeView.backgroundColor = [UIColor yellowColor];
-     break;
-     case 2:
-     awesomeView.backgroundColor = [UIColor greenColor];
-     break;
-     default:
-     awesomeView.backgroundColor = [UIColor blackColor];
-     break;
-     }
-     
-     [cvc.myScrollView addSubview:awesomeView];
-     }*/
     
     return cvc;
     
